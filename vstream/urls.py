adfbceddb4a18d819fab8839b9cf09e96bc8df5e
Askhab Musaev <42musaev@gmail.com>
2021-03-29 15:05:25 +0300

    update templates

--- vstream/urls.py
+++ vstream/urls.py
@@ -5,6 +5,6 @@ from vstream.views import Index, ListVideo, DetailVideo, stream_video
 urlpatterns = [
     path('', Index.as_view(), name='index'),
     path('list/', ListVideo.as_view(), name='list-view'),
-    path('video/<int:video_id>', DetailVideo.as_view()),
+    path('video/<int:video_id>', DetailVideo.as_view(), name='detail-video'),
     path('stream/video/<int:video_id>', stream_video, name='detail-video-stream')
 ]
