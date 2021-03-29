2c2bec074f164de85c63cc1c91ce2ec4540dce9a
Askhab Musaev <42musaev@gmail.com>
2021-03-29 11:50:45 +0300

    added models, media settings

--- vstream/admin.py
+++ vstream/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from vstream.models import Video
+
+
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    pass
--- vstream/models.py
+++ vstream/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Video(models.Model):
+    name = models.CharField(max_length=1024, blank=True)
+    video = models.FileField(unique='videos')
+
+    def __str__(self):
+        if self.name:
+            return self.name
+        return self.video.name
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
